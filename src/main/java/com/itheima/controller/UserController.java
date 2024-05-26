c51cd3340421786657dd7647deb45b708faba818
GU-JQR <20210312213@squ.edu.cn>
2024-05-26 10:11:20 +0800

    在getById上加注解@Cacheable

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -5,6 +5,7 @@ import com.itheima.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ public class UserController {
     }
 
     @DeleteMapping
+    @Cacheable(value="userCache",key="#id")
     public void deleteById(Long id){
         userMapper.deleteById(id);
     }
